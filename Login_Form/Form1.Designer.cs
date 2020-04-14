3af8ba7d37b6eee811d853c37b3fb40793300ee5
poornimayo <55318034+poornimayo@users.noreply.github.com>
2020-04-14 18:07:10 +0530

    Add files via upload

---
+++ Login_Form/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace Login_Form
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TextUsername = new System.Windows.Forms.TextBox();
+            this.TextPass = new System.Windows.Forms.TextBox();
+            this.Login = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(71, 77);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "USERNAME";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(71, 144);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "PASSWORD";
+            // 
+            // TextUsername
+            // 
+            this.TextUsername.Location = new System.Drawing.Point(232, 77);
+            this.TextUsername.Name = "TextUsername";
+            this.TextUsername.Size = new System.Drawing.Size(288, 22);
+            this.TextUsername.TabIndex = 2;
+            // 
+            // TextPass
+            // 
+            this.TextPass.Location = new System.Drawing.Point(232, 144);
+            this.TextPass.Name = "TextPass";
+            this.TextPass.Size = new System.Drawing.Size(288, 22);
+            this.TextPass.TabIndex = 3;
+            // 
+            // Login
+            // 
+            this.Login.Location = new System.Drawing.Point(275, 271);
+            this.Login.Name = "Login";
+            this.Login.Size = new System.Drawing.Size(211, 80);
+            this.Login.TabIndex = 4;
+            this.Login.Text = "Login";
+            this.Login.UseVisualStyleBackColor = true;
+            this.Login.Click += new System.EventHandler(this.Login_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Login);
+            this.Controls.Add(this.TextPass);
+            this.Controls.Add(this.TextUsername);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "TextPass";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox TextUsername;
+        private System.Windows.Forms.TextBox TextPass;
+        private System.Windows.Forms.Button Login;
+    }
+}
+
---
+++ Login_Form/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Login_Form
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Login_Click(object sender, EventArgs e)
+        {
+            String user, pass;
+            user = TextUsername.Text;
+            pass = TextPass.Text;
+            if (user == "admin" && pass == "admin")
+            {
+                MessageBox.Show("Successful login");
+
+            }
+            else
+            {
+                MessageBox.Show("error in login");
+            }
+        }
+    }
+}
